108287d394b6971dad3ccf2fea59a9c60ceb42a9
muhammeddishanv <21mcs05@meaec.edu.in>
2025-09-11 12:27:19 +0530

    removed all files

--- src/components/kitchen/KitchenDashboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import KitchenDisplaySystem from './KitchenDisplaySystem';
-
-const KitchenDashboard: React.FC = () => {
-  return <KitchenDisplaySystem />;
-};
-
-export default KitchenDashboard;
